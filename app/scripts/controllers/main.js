d33cb04a2a6cc693a0984afe08807d1b740c4a35
maciejmuchin <mmuchin@me.com>
2014-01-10 18:55:01 -0500

    angular CI inital commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularCiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
